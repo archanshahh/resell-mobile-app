3d9bd54ae419aa1d2765d9675f017d4b9c665379
archanshahh <archanshahh@gmail.com>
2020-05-31 18:22:25 -0400

    added picker and modal

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
@@ -6,6 +6,37 @@ import ListItem from "./app/components/ListItem";
 import MyAccountScreen from "./app/screens/AccountScreen";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import { TextInput, Text, Switch } from "react-native";
+import AppText from "./app/components/AppText";
+import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  {
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    label: "Clothing",
+    value: 2,
+  },
+  {
+    label: "Cameras",
+    value: 3,
+  },
+];
 export default function App() {
-  return <ListingsScreen />;
+  const [category, setCategory] = useState(categories[0]);
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectedItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
