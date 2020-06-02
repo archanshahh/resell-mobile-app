aeaa90a4b7be6e6490a57dc7b09be444534f6d4d
archanshahh <archanshahh@gmail.com>
2020-06-01 23:01:28 -0400

    added register Screen

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {} from "react-native";
+
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
+
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().min(2).label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+function RegisterScreen(props) {
+  return (
+    <Screen>
+      <AppForm
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          icon="account"
+          name="name"
+          placeholder="Name"
+          autoCorrect={false}
+        />
+        <AppFormField
+          icon="email"
+          name="email"
+          placeholder="Email"
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry
+        />
+
+        <SubmitButton title="Register" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+export default RegisterScreen;
